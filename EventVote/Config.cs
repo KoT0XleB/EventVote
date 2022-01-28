2d72030f2be914cc11645c3c79f88aeddf3540f1
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-01-28 14:03:25 +0500

    Add files via upload

---
+++ EventVote/Config.cs
@@ -0,0 +1,19 @@
+﻿using Qurre.API.Addons;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace EventVote
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "EventVote";
+
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+        [Description("Time vote in seconds. Default: 60?")]
+        public float Time { get; set; } = 60;
+        [Description("Url webhook: https://discord.com/api/webhooks/webhook.id/webhook.token")]
+        public string Token { get; set; } = string.Empty;
+    }
+}
